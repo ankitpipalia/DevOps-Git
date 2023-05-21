23bffa052a072d93a20fc1dc846a3722b8ec375a
Ankit Pipalia <67947828+ankitpipalia@users.noreply.github.com>
2023-05-21 20:24:19 +0530

    Update App.js

--- trainee_frontend/src/App.js
+++ trainee_frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Container>
               <Navbar.Brand>
                 <Link to={'/create-student'} className="nav-link">
-                  CRUD Operation
+                  MERN CRUD App
                 </Link>
               </Navbar.Brand>
 
