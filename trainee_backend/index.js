45e1c1e1a33f3196cc72caecb4b276d1c15b814d
Ankit Pipalia <ankit.pipalia009@gmail.com>
2023-11-07 20:21:39 +0530

    db url changed

--- trainee_backend/index.js
+++ trainee_backend/index.js
@@ -27,7 +27,7 @@ app.use('/students', studentRoute)
 
 
 // PORT
-const port = process.env.PORT || 4000;
+const port = 4000;
 const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
